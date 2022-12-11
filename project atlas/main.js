43a1b6c0c0e796a62331067282adb30a29c95d18
MayorEmpror <98406836+MayorEmpror@users.noreply.github.com>
2022-12-11 10:20:40 +0500

    Add files via upload

--- project atlas/main.js
+++ project atlas/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     // creepy stuff that just works
+    $(".atom").hide()
     window.scrollTo(0,0)
     $(".cover").on("click",function(){
-        $(this).addClass("animate__animated animate__fadeOutUp")
+        // $(this).addClass("animate__animated animate__fadeOutUp")
         // some more promise thingy that I don't know anything about
         const after = new Promise((r,rj) => {
-        $(".veil").slideUp(1000)
+         $(".veil").slideUp(1000)
         r()
         })
         // ??????
@@ -13,5 +14,32 @@ $(document).ready(function(){
             $("body").attr("style", "overflow-y: visible")
         })
     })
+    window.onbeforeunload = () => {
+        $("body").attr("style", "overflow:hidden")
+    window.scrollTo(0,0)
+    // return false
+    }
 
+
+    $(".home-page-button").on()
+      $(".home-page-button").on('keydown', function (event) {
+        if (event.key == "Escape") {
+          takeUserBack();
+        }
+       });
+   
+
+    function displayHomePage() {
+      $(".home-page").fadeIn(1000)
+      $("body").css({
+        overflow: "hidden"
+      })
+
+    }
+    function takeUserBack() {
+      $(".home-page").fadeOut(1000)
+      $("body").css({
+        overflow: "auto"
+      })
+    }
 })
